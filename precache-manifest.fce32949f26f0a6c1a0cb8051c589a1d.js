519099a404617f25744aa951a50762be3869d350
kishor g s <kishor.15082002@gmail.com>
2023-07-14 00:40:32 +0530

    Updates

---
+++ precache-manifest.fce32949f26f0a6c1a0cb8051c589a1d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6d1c7d266c9238c434",
+    "url": "/Kishor_g_s/static/css/main.94449aad.chunk.css"
+  },
+  {
+    "revision": "0e6d1c7d266c9238c434",
+    "url": "/Kishor_g_s/static/js/main.0e6d1c7d.chunk.js"
+  },
+  {
+    "revision": "efef7f6fa880e78d2a4c",
+    "url": "/Kishor_g_s/static/js/1.efef7f6f.chunk.js"
+  },
+  {
+    "revision": "8e62b3eb253517bd172e",
+    "url": "/Kishor_g_s/static/css/2.79d6b61d.chunk.css"
+  },
+  {
+    "revision": "8e62b3eb253517bd172e",
+    "url": "/Kishor_g_s/static/js/2.8e62b3eb.chunk.js"
+  },
+  {
+    "revision": "42ea5bb3721aacceebe3",
+    "url": "/Kishor_g_s/static/js/runtime~main.42ea5bb3.js"
+  },
+  {
+    "revision": "9cdf7e650a57a919b75382874c605110",
+    "url": "/Kishor_g_s/static/media/DSC_0693-01.9cdf7e65.jpeg"
+  },
+  {
+    "revision": "b21a8e5f660eeed3be0ff1276da7173a",
+    "url": "/Kishor_g_s/static/media/Coding.b21a8e5f.jpg"
+  },
+  {
+    "revision": "5a15c72600445082b6f5a2ff7c6b2ce8",
+    "url": "/Kishor_g_s/static/media/Music.5a15c726.jpeg"
+  },
+  {
+    "revision": "d1b083ac9d9344250908137e08d6eb20",
+    "url": "/Kishor_g_s/static/media/p.d1b083ac.jpg"
+  },
+  {
+    "revision": "91891be77ec016bb4c5541b028fb184e",
+    "url": "/Kishor_g_s/static/media/DSC_0218.91891be7.JPG"
+  },
+  {
+    "revision": "45ad5f8ec790f3119c3c37c5ba14eb8a",
+    "url": "/Kishor_g_s/static/media/DSC_0219.45ad5f8e.JPG"
+  },
+  {
+    "revision": "b71638f0d6770e0dccf95f04e3a91a32",
+    "url": "/Kishor_g_s/static/media/DSC_0522(1).b71638f0.jpg"
+  },
+  {
+    "revision": "4e398b91968dbed936a3513271eb8a0d",
+    "url": "/Kishor_g_s/index.html"
+  }
+];
